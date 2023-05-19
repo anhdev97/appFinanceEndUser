4d98949d6bbb476ca775dde56925e3c942943689
Ánh Nguyễn <anhdev97@gmail.com>
2023-05-19 23:14:55 +0700

    Config: Change config database

--- src/main/java/vn/an/finance/config/DatabaseConfiguration.java
+++ src/main/java/vn/an/finance/config/DatabaseConfiguration.java
@@ -1,16 +1,14 @@
 package vn.an.finance.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import tech.jhipster.config.JHipsterConstants;
 
 @Configuration
-@EnableJpaRepositories({ "vn.an.finance.repository" })
+@EntityScan({ "vn.an.finance.domain", "vn.an.finance.admin.domain" })
+@EnableJpaRepositories({ "vn.an.finance.repository", "vn.an.finance.admin.repository" })
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
 @EnableTransactionManagement
 public class DatabaseConfiguration {}
